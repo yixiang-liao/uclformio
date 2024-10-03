1c2c5ea79d03cfcc696ca8b625a1b57d8fd444cc
yixiangliao <ah6020308@gmail.com>
2024-10-03 21:13:26 +0800

    2024/10/03 21:15

--- uclform/src/pages/FormPage1.js
+++ uclform/src/pages/FormPage1.js
@@ -3,7 +3,8 @@ import DynamicForm from '../components/DynamicForm';
 
 const FormPage1 = () => (
   <div>
-    <DynamicForm apiUrl="https://wemedia.54ucl.com/formioopensources/projectestimate1" title="工程估價單1" />
+    {/* <DynamicForm apiUrl="https://wemedia.54ucl.com/formioopensources/projectestimate1" title="工程估價單1" /> */}
+    <DynamicForm apiUrl="http://127.0.0.1:8000/api/v1/projectestimate1" title="工程估價單1" />
   </div>
 );
 
--- uclform/src/pages/FormPage2.js
+++ uclform/src/pages/FormPage2.js
@@ -2,8 +2,8 @@ import React from 'react';
 import DynamicForm from '../components/DynamicForm';
 
 const FormPage2 = () => (
-  <DynamicForm apiUrl="https://wemedia.54ucl.com/formioopensources/projectestimate2" title="工程估價單2" />
-  // <DynamicForm apiUrl="../json/projectestimate2.json" title="工程估價單2" />
+  // <DynamicForm apiUrl="https://wemedia.54ucl.com/formioopensources/projectestimate2" title="工程估價單2" />
+  <DynamicForm apiUrl="http://127.0.0.1:8000/api/v1/projectestimate2" title="工程估價單1" />
 );
 
 export default FormPage2;
