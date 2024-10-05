7f3ddfe4765a5d428c1cd4deb645e68a71e8e29d
yixiangliao <ah6020308@gmail.com>
2024-10-05 16:58:47 +0800

    一小步的成功，大大的開心，終於解決啦！！！剩下一小步

--- uclform/src/components/DynamicForm.js
+++ uclform/src/components/DynamicForm.js
@@ -1,14 +1,32 @@
 
-import React from 'react'
+// import React , { useEffect, useRef } from 'react'
+// import { Form } from 'react-formio';
+// import 'formiojs/dist/formio.full.css';
+
+// const DynamicForm = ({apiUrl}) => {
+//   return (
+//     <div className='formio-form'>
+//       <Form src={apiUrl} className='formio-form'/>
+//     </div>
+//   )
+// }
+
+// export default DynamicForm
+
+import React, { useEffect, useRef } from 'react';
 import { Form } from 'react-formio';
 import 'formiojs/dist/formio.full.css';
+import Formio from 'formiojs/Formio';
 
-const DynamicForm = ({apiUrl}) => {
+const DynamicForm = ({ apiUrl, title }) => {
   return (
-    <div className='formio-form'>
-      <Form src={apiUrl} className='formio-form'/>
+    <div>
+      <h1>{title}</h1>
+      <div className='formio-form'>
+        <Form src={apiUrl} className='formio-form'/>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default DynamicForm
+export default DynamicForm;
--- uclform/src/pages/FormPage2.js
+++ uclform/src/pages/FormPage2.js
@@ -3,33 +3,7 @@ import DynamicForm from '../components/DynamicForm';
 
 const FormPage2 = () => (
   // <DynamicForm apiUrl="https://wemedia.54ucl.com/formioopensources/projectestimate2" title="工程估價單2" />
-  <DynamicForm apiUrl="http://127.0.0.1:8000/api/v1/projectestimate2" title="工程估價單1" />
+  <DynamicForm apiUrl="http://127.0.0.1:8000/api/v1/projectestimate2" title="工程估價單2" />
 );
 
 export default FormPage2;
-
-// import React, { useEffect, useState } from 'react';
-// import DynamicForm from '../components/DynamicForm';
-
-// const FormPage2 = () => {
-//   const [formSchema, setFormSchema] = useState(null);
-
-//   useEffect(() => {
-//     fetch('../json/projectestimate2.json')
-//       .then((response) => response.json())
-//       .then((data) => {
-//         setFormSchema(data);
-//       })
-//       .catch((error) => {
-//         console.error('Error loading form schema:', error);
-//       });
-//   }, []);
-
-//   return formSchema ? (
-//     <DynamicForm formSchema={formSchema} title="工程估價單2" />
-//   ) : (
-//     <p>Loading...</p>
-//   );
-// };
-
-// export default FormPage2;
